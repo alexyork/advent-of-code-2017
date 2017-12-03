8e86c6aafa3ee8196658d830e6843a28390646ca
Alex York <alexyork1985@gmail.com>
2017-12-03 12:04:03 +0100

    Whitespace + formatting

--- src/01.js
+++ src/01.js
@@ -1,40 +1,40 @@
 export function getCaptchaResult(captchaInput) {
-    let sum = 0;
+  let sum = 0;
 
-    for (let i = 0; i < captchaInput.length; i++) {
-        const currentDigit = parseInt(captchaInput[i], 10);
+  for (let i = 0; i < captchaInput.length; i++) {
+    const currentDigit = parseInt(captchaInput[i], 10);
 
-        const maybeNextDigit = captchaInput[i + 1];
-        const nextDigit = (maybeNextDigit)
-            ? parseInt(maybeNextDigit, 10)
-            : parseInt(captchaInput[0], 10);
+    const maybeNextDigit = captchaInput[i + 1];
+    const nextDigit = (maybeNextDigit)
+      ? parseInt(maybeNextDigit, 10)
+      : parseInt(captchaInput[0], 10);
 
-        if (currentDigit === nextDigit) {
-            sum += currentDigit;
-        }
+    if (currentDigit === nextDigit) {
+      sum += currentDigit;
     }
+  }
 
-    return sum;
+  return sum;
 }
 
 export function getCaptchaResult2(captchaInput) {
-    const doubledCaptchaInput = captchaInput + captchaInput; // "foo" -> "foofoo"
+  const doubledCaptchaInput = captchaInput + captchaInput; // "foo" -> "foofoo"
 
-    let sum = 0;
+  let sum = 0;
 
-    for (let i = 0; i < captchaInput.length; i++) {
-        const currentDigit = parseInt(captchaInput[i], 10);
-        const digitsAheadWeMustLook = (captchaInput.length / 2);
-        const potentialHalfwayAroundDigit = captchaInput[i + digitsAheadWeMustLook]; // Might not exist, if it overflows
+  for (let i = 0; i < captchaInput.length; i++) {
+    const currentDigit = parseInt(captchaInput[i], 10);
+    const digitsAheadWeMustLook = (captchaInput.length / 2);
+    const potentialHalfwayAroundDigit = captchaInput[i + digitsAheadWeMustLook]; // Might not exist, if it overflows
 
-        const halfwayAroundDigit = (potentialHalfwayAroundDigit)
-            ? parseInt(potentialHalfwayAroundDigit, 10)
-            : parseInt(doubledCaptchaInput[i + digitsAheadWeMustLook], 10);
+    const halfwayAroundDigit = (potentialHalfwayAroundDigit)
+      ? parseInt(potentialHalfwayAroundDigit, 10)
+      : parseInt(doubledCaptchaInput[i + digitsAheadWeMustLook], 10);
 
-        if (currentDigit === halfwayAroundDigit) {
-            sum += currentDigit;
-        }
+    if (currentDigit === halfwayAroundDigit) {
+      sum += currentDigit;
     }
+  }
 
-    return sum;
+  return sum;
 }
--- src/02.js
+++ src/02.js
@@ -1,45 +1,45 @@
 import _ from 'lodash';
 
 export function getSpreadsheetChecksum(spreadsheetInput) {
-    const rows = spreadsheetInput.split('\n').map(r => r.trim()).filter(r => !!r);
+  const rows = spreadsheetInput.split('\n').map(r => r.trim()).filter(r => !!r);
 
-    let checksum = 0;
-    rows.forEach(row => {
-        const cells = row.trim().split(',').map(c => parseInt(c, 10));
-        const sortedCells = _.sortBy(cells);
+  let checksum = 0;
+  rows.forEach(row => {
+    const cells = row.trim().split(',').map(c => parseInt(c, 10));
+    const sortedCells = _.sortBy(cells);
 
-        const smallestCellValue = sortedCells[0];
-        const largestCellValue = sortedCells[sortedCells.length - 1];
-        const difference = (largestCellValue - smallestCellValue);
-        checksum += difference;
-    });
-    return checksum;
+    const smallestCellValue = sortedCells[0];
+    const largestCellValue = sortedCells[sortedCells.length - 1];
+    const difference = (largestCellValue - smallestCellValue);
+    checksum += difference;
+  });
+  return checksum;
 }
 
 export function getSpreadsheetChecksum2(spreadsheetInput) {
-    const rows = spreadsheetInput.split('\n').map(r => r.trim()).filter(r => !!r);
+  const rows = spreadsheetInput.split('\n').map(r => r.trim()).filter(r => !!r);
 
-    let checksum = 0;
-    rows.forEach(row => {
-        const cells = row.trim().split(',').map(c => parseInt(c, 10));
-        const sortedCells = _.sortBy(cells);
-        let evenlyDivisibleNumbersFound = false;
+  let checksum = 0;
+  rows.forEach(row => {
+    const cells = row.trim().split(',').map(c => parseInt(c, 10));
+    const sortedCells = _.sortBy(cells);
+    let evenlyDivisibleNumbersFound = false;
 
-        for (let i = 0; i < sortedCells.length; i++) {
-            if (evenlyDivisibleNumbersFound) break;
+    for (let i = 0; i < sortedCells.length; i++) {
+      if (evenlyDivisibleNumbersFound) break;
 
-            for (let k = (i + 1); k < sortedCells.length; k++) {
-                if (evenlyDivisibleNumbersFound) break;
+      for (let k = (i + 1); k < sortedCells.length; k++) {
+        if (evenlyDivisibleNumbersFound) break;
 
-                const divisionResult = (sortedCells[k] / sortedCells[i]);
-                const wasEvenlyDivisible = (divisionResult % 1) === 0;
+        const divisionResult = (sortedCells[k] / sortedCells[i]);
+        const wasEvenlyDivisible = (divisionResult % 1) === 0;
 
-                if (wasEvenlyDivisible) {
-                    evenlyDivisibleNumbersFound = true;
-                    checksum += divisionResult;
-                }
-            }
+        if (wasEvenlyDivisible) {
+          evenlyDivisibleNumbersFound = true;
+          checksum += divisionResult;
         }
-    });
-    return checksum;
+      }
+    }
+  });
+  return checksum;
 }
